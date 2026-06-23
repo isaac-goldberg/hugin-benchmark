928e5b476d0ab6050062fc98695fcafa89fae6d3
isaac-goldberg <isaacg0ldb3rg@gmail.com>
2026-06-23 11:24:42 -0700

    Initial Commit

---
+++ hugin-benchmark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hugin-benchmark
+//
+//  Created by Isaac Goldberg on 6/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hugin-benchmark/hugin_benchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  hugin_benchmarkApp.swift
+//  hugin-benchmark
+//
+//  Created by Isaac Goldberg on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct hugin_benchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
